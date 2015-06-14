ae978f3675c680f88291de9adcbe618188f6cf46
luzpaz <luzpaz@users.noreply.github.com>
2015-06-14 12:27:59 -0500

    added libfreehand dep

--- scribus.rb
+++ scribus.rb
@@ -36,6 +36,7 @@ class Scribus < Formula
   depends_on 'libmspub' => :recommended         # Importer: MS-Publisher
   depends_on 'libcdr' => :recommended           # Importer: Corel Draw
   depends_on 'libpagemaker' => :recommended     # Importer: Adobe Pagemaker
+  depends_on 'libfreehand' => :recommended      # Importer: Aldus/Macromedia/Adobe Freehand
   depends_on 'open-scene-graph' => :recommended # 3D PDF Annotations
 
    def install
